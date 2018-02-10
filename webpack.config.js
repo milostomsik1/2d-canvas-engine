bdab3143c3b869fe5268c4a8709aa72421db6689
Milos Tomsik <milostomsik1@gmail.com>
2018-02-10 15:31:41 +0100

    init

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const config = {
+  entry: './src/app.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  },
+  watch: true
+};
+
+module.exports = config;
