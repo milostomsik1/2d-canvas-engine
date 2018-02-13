38688c5855a860f603e68885976c184a89e89a53
Milos Tomsik <milostomsik1@gmail.com>
2018-02-13 11:06:29 +0100

    cleaned up shapes code, moved to abstract class

--- src/Shapes/square.ts
+++ src/Shapes/square.ts
@@ -7,44 +7,6 @@ class Square extends AbstractShape {
     side: 100
   }
 
-  public get x(): number {
-    return this._x;
-  }
-
-  public set x(x: number) {
-    if (typeof x === 'number') {
-      this._x = x;
-    } else {
-      throw new Error('X coordinate must be a number.');
-    }
-  }
-
-  public get y(): number {
-    return this._y;
-  }
-
-  public set y(y: number) {
-    if (typeof y === 'number') {
-      this._y = y;
-    } else {
-      throw new Error('Y coordinate must be a number.');
-    }
-  }
-
-  public set originPoint(point: Point) {
-    if (point.hasOwnProperty('x') &&
-        point.hasOwnProperty('y')) {
-      if (point.x >= 0 && point.x <= 1 &&
-          point.y >= 0 && point.y <= 1) {
-        this._originPoint = point;
-      } else {
-        throw new Error('Incorrect point value. X and Y must be between 0 and 1.');
-      }
-    } else {
-      throw new Error('Incorrect point format. Must have x and y properties.');
-    }
-  }
-
   public get dimensions(): SquareDimensions {
     return this._dimensions;
   }
